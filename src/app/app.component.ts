943d836e79f1d0368e79457b296183354e04759f
John Yu <johnyu.programmer@gmail.com>
2025-02-28 15:53:55 -0800

    add scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
-import { Amplify } from 'aws-amplify';
-import outputs from '../../amplify_outputs.json';
-
-Amplify.configure(outputs);
+// import { Amplify } from 'aws-amplify';
+// import outputs from '../../amplify_outputs.json';
+// Amplify.configure(outputs);
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.scss',
   imports: [RouterOutlet, TodosComponent],
 })
 export class AppComponent {
