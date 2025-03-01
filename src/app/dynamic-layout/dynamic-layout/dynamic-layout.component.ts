71c5d0eac48ed586d7583cb13b30dc12d264a4fe
John Yu <johnyu.programmer@gmail.com>
2025-02-28 16:05:34 -0800

    add boilerplate

---
+++ src/app/dynamic-layout/dynamic-layout/dynamic-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './dynamic-layout.component.html',
+  styleUrl: './dynamic-layout.component.scss'
+})
+export class DynamicLayoutComponent {
+
+}
---
+++ src/app/fallback/fallback/fallback.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fallback',
+  standalone: true,
+  imports: [],
+  templateUrl: './fallback.component.html',
+  styleUrl: './fallback.component.scss'
+})
+export class FallbackComponent {
+
+}
