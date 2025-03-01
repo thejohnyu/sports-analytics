967150cb3a2bd99f0acfb050ea544a4f0a07a5f4
John Yu <johnyu.programmer@gmail.com>
2025-02-28 16:33:48 -0800

    redo the entire root application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TodosComponent } from './todos/todos.component';
+import { HeaderComponent } from './header/header/header.component';
 // import { Amplify } from 'aws-amplify';
 // import outputs from '../../amplify_outputs.json';
 // Amplify.configure(outputs);
@@ -10,7 +10,7 @@ import { TodosComponent } from './todos/todos.component';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, TodosComponent],
+  imports: [RouterOutlet, HeaderComponent],
 })
 export class AppComponent {
   title = 'amplify-angular-template';
